c6e4d71ae2348ba6fa69b045d67d9968da2237a1
Simple-temp <fiveriam01@gmail..com>
2021-12-24 22:41:38 -1200

    assignment doen

--- src/component/ShowCommens/ShowCommens.js
+++ src/component/ShowCommens/ShowCommens.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './ShowCommens.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import { useState } from 'react';
 
 const ShowCommens = (props) => {
+
+    const [Like, setLike] = useState('');
+
     const {comment} = props;
     const {name,body,postId} = comment;
     return (
-        <div className='row row-inner'>
-            <div className="col-lg-2 col-md-2 col-sm-2 d-flex justify-content-center align-items-center">
+        <div className='row mt-3'>
+            <div className="col-lg-1 col-md-1 col-sm-1">
                 <img src={"https://imagizer.imageshack.com/img924/2149/K5ZlUR.png"} className='img' alt="" />
             </div>
-            <div className="col-lg-8 col-md-6 col-sm-6 p">
+            <div className="col-lg-6 col-md-6 col-sm-6 p row-inner">
                 <p> <b>Name</b>  : {name}</p>
                 <p> <b>Comment</b>  : {body}</p>
+                <ThumbUpIcon onClick={()=>setLike(Like ? '' : 'primary')} color={Like} ></ThumbUpIcon>
             </div>
         </div>
     );
